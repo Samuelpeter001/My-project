18fd23154ab14a531e7b5835b94c12d9dd6b13ea
samuelpeter001 <samuelpeter61@gmail.com>
2025-02-21 14:35:27 +0100

    added codes

--- script/web.js
+++ script/web.js
@@ -1,6 +1,6 @@
 const menu = document.querySelector('#Menu');
 const menuLinks = document.querySelector('.menu_tab');
-
+ 
 menu.addEventListener('click', function() {
     menu.classList.toggle('is-active');
     menuLinks.classList.toggle('active');
